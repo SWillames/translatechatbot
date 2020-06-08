9ec66d1c8c2f6740a70eac9fa95aa40ff256bf23
SWillames <sergiowoc@gmail.com>
2020-06-08 11:35:18 -0300

    Improving the tests.

--- spec/services/translate_service_spec.rb
+++ spec/services/translate_service_spec.rb
@@ -1,39 +1,113 @@
 require_relative './../spec_helper.rb'
+require "./app/errors/errors.rb"
  
 describe TranslateBot::TranslateService do
-  before do
-    @text = "Vaca"
-    @lang = "ingles"
-    @translate = "the cow"
-  end
- 
+   
   describe '#call' do
-    context "Without text param" do
-      it "will receive a error" do
-        @translateService = TranslateBot::TranslateService.new({"lang" => @lang})
-        response = @translateService.call()
-        expect(response).to match("Informe algum texto para traduzir.")
-      end
+    let(:text) {FFaker::Lorem.word}
+    let(:text_translated) {FFaker::Lorem.word}
+    let(:source_lang) { FFaker::Locale.language('random')}
+    let(:target_lang) {FFaker::Locale.language('random')}
+    
+    context "Error translating" do
+       
+        context "Bad Request" do
+
+          let(:response_of_api)do
+            {
+              code: 400,
+              message: 'Error'
+            }
+          end
+          before do
+            allow(RestClient).to receive(:post) { response_of_api.to_json }
+          end
+             
+            context "Without some param" do
+              it "will receive a error" do
+                @translateService = TranslateBot::TranslateService.new({"text" => text, "source_lang" => source_lang})
+                response = @translateService.call()
+                format_translat = "1- Texto a ser traduzido, 2- idioma de origem, 3- idioma final"
+                expect(response).to match("Falta algum dos parametros para tradução: #{format_translat}")
+              end
+            end
+        end
+
+        context "Internal error" do
+            
+                let(:response_of_api)do
+                    {
+                        code: rand(401..402),
+                        message: 'Error'
+                    }
+                end
+
+                before do
+                    allow(RestClient).to receive(:post) { response_of_api.to_json }
+                end
+
+                it "Error of comunicate" do
+                    message = 'Error when trying to communicate with API.'
+                    expect{ 
+                        TranslateBot::TranslateService.new({
+                            'text' => text, 
+                            'source_lang' => source_lang, 
+                            'target_lang' => target_lang}).call()
+                    }.to raise_error(Errors::ErrorComunication, message)
+                end
+        end
+
+        context "Text too long" do
+                
+                let(:text_long) {FFaker::Lorem.characters(character_count = 10001)}
+                let(:response_of_api)do
+                    {
+                        code: 413,
+                        message: 'Text error too long.'
+                    }
+                end
+
+                before do
+                    allow(RestClient).to receive(:post) {response_of_api.to_json}
+                end
+
+                it "raise a ApiTextMaxSizeError" do
+                    message = "A text #{text_long} exceed a 10.000 characters."
+                    expect{ 
+                        TranslateBot::TranslateService.new({
+                            'text' => text_long, 
+                            'source_lang' => source_lang, 
+                            'target_lang' => target_lang}).call() 
+                    }.to raise_error(Errors::ErrorTextLong, message)
+                end
+        end    
     end
 
-    context "Without lang param" do
-      it "will receive a error" do
-        @translateService = TranslateBot::TranslateService.new({"text" => @text})
-        response = @translateService.call()
-        expect(response).to match("Informe o idioma.")
+    context "Success" do
+      
+      let(:response_of_api)do
+        {
+          code: 200,
+          lang: "#{source_lang}-#{target_lang}",
+          text: [
+              text_translated
+          ]
+        }
       end
-    end
- 
-    context "With Valid params" do
-      before do
-        @translateService = TranslateBot::TranslateService.new({"text" => @text, "lang" => @lang})
-        @response = @translateService.call()
-      end
- 
-      it "Receive success message" do
-        expect(@response).to match("O termo #{@text} traduzido para #{@lang} é: #{@translate}")
+
+        before do
+          allow(RestClient).to receive(:post) {response_of_api.to_json}
+          @response = TranslateBot::TranslateService.new({
+                      'text' => text, 
+                      'source_lang'=> source_lang, 
+                      'target_lang' => target_lang}).call()
+        end
+
+        it "will receive a translated text" do
+             expect(@response).to match("#{text_translated}")
+        end
+
       end
 
     end
   end
-end
