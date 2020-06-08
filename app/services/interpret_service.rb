a69c00652dd37dd86d1158d77c147e073061b97f
SWillames <sergiowoc@gmail.com>
2020-06-08 13:28:06 -0300

    Interpret service created.

--- app/services/interpret_service.rb
+++ app/services/interpret_service.rb
@@ -1,1 +1,16 @@
-interpret_service.rb
+class InterpretService
+
+    def self.call action, params
+        case action
+        when 'get_languages'
+            TranslateModule::LanguageAvailable.new().call()
+        when 'translate'
+            TranslateModule::TranslateService.new(params).call()
+        when 'help'
+            HelpService.call()
+        else
+            'Não entendi o que você quer'
+        end    
+    end
+
+end
